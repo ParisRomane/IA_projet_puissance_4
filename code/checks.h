150eb5929a124bce44e807c11eb5d00e6dd8766d
Yvregon <steph.ade17@gmail.com>
2023-02-02 12:53:30 +0100

    First attempt of victory check functions. Bugged as usual

---
+++ code/checks.h
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "State.h"
+
+end_e switch_stmt(int statement, int & acc_hu, int & acc_ai){
+
+    switch (statement){
+        case HU_CROSS:
+            acc_hu ++;
+            acc_ai = 0;
+        break;
+        
+        case AI_ROUND:
+            acc_hu = 0;
+            acc_ai ++;
+        break;
+    
+        default:    //VOID
+            return NONE;
+        break;
+    }
+
+    if(acc_hu == 4){
+        return HU_VICTORY;
+    }
+
+    if(acc_ai == 4){
+        return AI_VICTORY;
+    }
+}
+
+end_e check_upward_diag(State state, int x, int y){
+    int acc_hu = 0;
+    int acc_ai = 0;
+
+    for(int i = x; i >= x; i++){
+        for(int j = HEIGHT; j >= y; j--){
+
+            end_e res = switch_stmt(state.board[j*WIDTH + i], acc_hu, acc_ai);
+
+            if(res == HU_VICTORY && AI_VICTORY){
+                return res;
+            }
+        }
+    }
+    
+    return NONE;
+}
+
+end_e check_downward_diag(State state, int x, int y){
+
+    int acc_hu = 0;
+    int acc_ai = 0;
+
+    for(int i = x; i >= x; i++){
+        for(int j = y; j < HEIGHT; j++){
+
+            end_e res = switch_stmt(state.board[j*WIDTH + i], acc_hu, acc_ai);
+            
+            if(res == HU_VICTORY && AI_VICTORY){
+                return res;
+            }
+        }
+    }
+
+    return NONE;
+}
+
+end_e check_line(State state, int y){
+
+    int acc_hu = 0;
+    int acc_ai = 0;
+
+    for(int i = 0; i < WIDTH; i++){
+
+        end_e res = switch_stmt(state.board[y*WIDTH + i], acc_hu, acc_ai);
+            
+        if(res == HU_VICTORY && AI_VICTORY){
+            return res;
+        }
+    }
+
+    return NONE;
+}
+
+end_e check_column(State state, int x){
+
+    int acc_hu = 0;
+    int acc_ai = 0;
+
+    for(int i = 0; i < HEIGHT; i++){
+
+        end_e res = switch_stmt(state.board[i*WIDTH + x], acc_hu, acc_ai);
+            
+        if(res == HU_VICTORY && AI_VICTORY){
+            return res;
+        }
+    }
+
+    return NONE;
+}
